0709a085fa86aa2af93495c554ad80bf57134440
detectivecrow2540 <detectivecrow2540@gmail.com>
2023-03-21 23:02:03 +0900

    feat: add open ai call feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 const { context } = require('@actions/github');
 const { Octokit } = require('@octokit/rest');
+const { Configuration, OpenAIApi } = require("openai");
 
-const MAX_PATCH_COUNT = 4000;
+const MAX_PATCH_COUNT = 3000;
+
+async function initOpenAI(key) {
+  // https://platform.openai.com/docs/api-reference/completions/create
+  const configuration = new Configuration({
+    apiKey: key,
+  });
+  return new OpenAIApi(configuration);
+}
+
+// about model : https://platform.openai.com/docs/models/overview
+async function codeReview(openAI, code, model = 'gpt-3.5-turbo') {
+  if (!code) {
+    return '';
+  }
+
+  const language = process.env.LANGUAGE ?  `Answer me in ${process.env.LANGUAGE}` : '';
+  const message = `Please brief code review below code patch, ${language}
+  ${code}
+  `;
+
+  console.log(`start chat, message is "${message}"`);
+
+  try {
+    const response = await openAI.createChatCompletion({
+      model: model,
+      messages: [{ role: 'user', content: String(message) }],
+      max_tokens: MAX_PATCH_COUNT,
+      temperature: 1,
+      stream: false,
+    });
+
+    console.log(`response received! "${response.data.choices[0].message.content}"`);
+
+    return response.data.choices[0].message.content;
+  } catch (error) {
+    console.error(error)
+
+    return error;
+  }
+}
 
 async function run() {
   // Create octokit instance (bring context from github token)
@@ -11,8 +52,10 @@ async function run() {
 
   const { owner, repo } = context.repo;
 
-  // TODO : Create new chat instance
+  // Create new chat instance
+  const openAI = await initOpenAI(process.env.OPENAI_API_KEY);
 
+  // Get changed files
   const { data: compareCommits } = await octokit.repos.compareCommits({
     owner: owner,
     repo: repo,
@@ -38,13 +81,17 @@ async function run() {
     );
   }
 
+  console.log("changed file length is " + changedFiles?.length);
+
   if (!changedFiles?.length) {
     return 'there is no change';
   }
 
   console.time('gpt cost');
 
+  // Review code
   for (let i = 0; i < changedFiles.length; i++) {
+    console.log(`review (${i + 1}/${changedFiles.length})`);
     const file = changedFiles[i];
     const patch = file.patch || '';
 
@@ -56,23 +103,24 @@ async function run() {
       continue;
     }
 
-    // TODO : Get response from chat instance
+    // Get response from chat instance
+    const response = await codeReview(openAI, String(patch))
 
-    // await octokit.pulls.createReviewComment({
-    //   repo: repo,
-    //   owner: owner,
-    //   pull_number: context.payload.pull_request.number,
-    //   commit_id: commits[commits.length - 1].sha,
-    //   path: file.filename,
-    //   body: "this is test comment",
-    //   position: patch.split('\n').length - 1,
-    // });
-
-    console.timeEnd('gpt cost');
-    console.info('suceess reviewed', context.payload.pull_request.html_url);
-
-    return 'complete';
+    await octokit.pulls.createReviewComment({
+      repo: repo,
+      owner: owner,
+      pull_number: context.payload.pull_request.number,
+      commit_id: commits[commits.length - 1].sha,
+      path: file.filename,
+      body: response,
+      position: patch.split('\n').length - 1,
+    });
   }
+
+  console.timeEnd('gpt cost');
+  console.info('suceess reviewed', context.payload.pull_request.html_url);
+
+  return 'complete';
 }
 
 run();
